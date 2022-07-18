3a712413b8c924427b0630f2cab42be52515ffb9
MarcinBanas <mb056625@student.ath.edu.pl>
2022-07-19 00:26:15 +0200

    commit

--- Proj1/Models/Proj1Context.cs
+++ Proj1/Models/Proj1Context.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using System.Configuration;
-
+using System.Linq;
 
 namespace Proj1.Models
 {
@@ -83,5 +83,74 @@ namespace Proj1.Models
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+        public void DodajAktywo()
+        {
+            
+            Console.WriteLine("Podaj nazwe aktywa:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Podaj kod aktywa:");
+            string kod = Console.ReadLine();
+            var Aktywo = new Aktywa()
+            {
+                NazwaAktywa = name,
+                KodAktywa=kod
+            };
+            Aktywas.Add(Aktywo);
+            Console.WriteLine($"Dodano aktywo:({name}) kod:({kod})");
+        }
+        public void UsunAktywo()
+        {
+
+            Console.WriteLine("Podaj nazwe aktywa do usunięcia:");
+            string? name = Console.ReadLine();
+            var aktywoDoUsuniecia = Aktywas.Where(a => a.NazwaAktywa == name).ToList();
+            if (aktywoDoUsuniecia.Count() == 0)
+            {
+                Console.WriteLine("Nie znaleziono dokładnej nazwy aktywa");  
+            }
+            if (aktywoDoUsuniecia.Count()>1)
+            {
+                Console.WriteLine("Podaj dokladne id");
+                foreach (var item in aktywoDoUsuniecia)
+                {
+                    Console.WriteLine($"{item.Idaktywa}, {item.NazwaAktywa}, {item.KodAktywa}");
+                }
+                Console.WriteLine();
+                var id = Convert.ToInt32(Console.ReadLine());
+                foreach (var nrid in aktywoDoUsuniecia)
+                {
+                    if (nrid.Idaktywa == id)
+                    {
+                        Console.WriteLine($"czy napewno chcesz usunąć aktywo o id:{id} T/N");
+                        var TakNie = Console.ReadLine();
+                        if (TakNie == "T")
+                        {
+                            var aktywo = Aktywas.First(i => i.Idaktywa == id);
+                            Aktywas.Remove(aktywo);
+                            Console.WriteLine("Usunieto");
+                            return;
+                        }
+                        return;
+                    }
+                }
+                 Console.WriteLine("zly nr id!");
+            }
+            if(aktywoDoUsuniecia.Count() == 1)
+            {
+                var aktywo = Aktywas.First(i => i.NazwaAktywa == name);
+                Console.WriteLine($"czy napewno chcesz usunąć aktywo o nazwie:{name} T/N");
+                var TakNie = Console.ReadLine();
+                if (TakNie == "T")
+                {
+                    Aktywas.Remove(aktywo);
+                    Console.WriteLine("Usunieto");
+                    return;
+                }
+                return;
+            }
+            
+            
+        }
     }
 }
--- Proj1/Program.cs
+++ Proj1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Proj1.Models;
+Console.WriteLine("Witaj");
+
+var context = new Proj1Context();
+
+int wybór = 0;
+while (wybór != 5)
+{
+    Console.WriteLine("1-Dodaj aktywo");
+    Console.WriteLine("2-Usuń aktywo");
+    Console.Write($"Wybierz co chcesz zrobić: ");
+    wybór = Int32.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    switch (wybór)
+    {
+        case 1:
+            context.DodajAktywo();
+            context.SaveChanges();
+            break;
+        case 2:
+            context.UsunAktywo();
+            context.SaveChanges();
+            break;
+        default:
+            break;
+    }
+}
